27d263d805360c251af7f6113fe27355f293aa0d
jgranizod <joaquingranizo06@gmail.com>
2026-02-10 10:49:35 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AIzaSyAnwcdRp0V_9cBVPEXmG2IItV8uz7xUyE4",
   authDomain: "catalogoflores.firebaseapp.com",
   projectId: "catalogoflores",
   storageBucket: "catalogoflores.firebasestorage.app",
